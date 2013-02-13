904b4b3e4d552ddacedba24caf8c3e09df257494
Shane Rogers <rogerssh@tcd.ie>
2013-02-13 16:30:55 -0700

    push transactions & invoice items

---
+++ lib/invoice_item.rb
@@ -0,0 +1,17 @@
+require 'csv'
+require 'time'
+ 
+class InvoiceItem
+
+  attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  def initialize(input)
+    @id = input["id"]
+    @item_id = input["item_id"]
+    @invoice_id = input["merchant_id"]
+    @status = input["status"]
+    @created_at = Time.parse(input["created_at"])
+    @updated_at = Time.parse(input["updated_at"])
+  end
+
+end
