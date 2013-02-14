5fb8fd9355828613b149d2a6c7112bf5c86c0872
Shane Rogers <rogerssh@tcd.ie>
2013-02-13 17:14:08 -0700

    trns & incoice item

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,12 @@
 require 'csv'
 require 'time'
- 
-class Transaction
 
+
+class Transactions
   attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at
- 
+
   def initialize(input)
+    puts input
     @id = input["id"]
     @invoice_id = input["invoice_id"]
     @credit_card_number = input["credit_card_number"]
@@ -15,4 +16,18 @@ class Transaction
     @updated_at = Time.parse(input["updated_at"])
   end
 
+  def to_s
+    "#{@id} #{@invoice_id} #{@credit_card_number} #{@credit_card_expiration_date} #{@result} #{@created_at} #{@updated_at}"
+  end
+
+ #  def self.load
+ #    @invoices = LoadFiles.new.load_invoices_file
+ #    puts @invoices
+ # end
+
+  # def self.all
+  #   @invoices
+  # end
 end
+
+# transactions & invoice_items
